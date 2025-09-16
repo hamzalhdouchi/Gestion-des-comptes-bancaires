252a14307f06f5299d1c2af9a53f6cf632d6839c
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-16 11:31:47 +0100

    creation de class Dbconnection et les attr nésseser

--- src/dbConnection/Dbconnection.java
+++ src/dbConnection/Dbconnection.java
@@ -2,3 +2,17 @@ package dbConnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+package dbConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Dbconnection {
+    private static Dbconnection instance = null;
+    private static Connection connection;
+    public static final String URL = "jdbc:mysql://localhost:3306/gestion_des_compte_bancaires?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "37533753";
+
+}
