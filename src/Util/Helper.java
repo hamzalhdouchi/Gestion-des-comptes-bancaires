7e6d1247adfeca8bdd10ec9b0e93c95713d3da95
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-16 19:57:31 +0100

    create methode pour genere le UUID

---
+++ src/Util/Helper.java
@@ -0,0 +1,15 @@
+package Util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+import java.util.UUID;
+
+public class Helper {
+
+    public static String genererUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+
+}
