d5f2992cceec7694e28d4366ff06f8669eb67d0e
hamzalhadouchi <hamzabniss16@gmail.com>
2025-09-16 11:37:46 +0100

    creation de constructeur avec class for name pour import jdbc drive et handel les exeption try catch

--- src/dbConnection/Dbconnection.java
+++ src/dbConnection/Dbconnection.java
@@ -3,16 +3,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-package dbConnection;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Dbconnection {
     private static Dbconnection instance = null;
     private static Connection connection;
     public static final String URL = "jdbc:mysql://localhost:3306/gestion_des_compte_bancaires?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "37533753";
+
+    private Dbconnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connexion réussie !");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver JDBC non trouvé.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Erreur de connexion à la base de données.");
+            e.printStackTrace();
+        }
+    }
 
 }
